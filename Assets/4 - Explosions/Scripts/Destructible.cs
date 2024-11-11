dddc6aead89dbddcd2fb2db79ae8a66f194d35f5
Marlus <marlusvinicius2014@outlook.com>
2024-11-11 15:41:14 -0300

    add tesxt feedback to crate damage

--- Assets/4 - Explosions/Scripts/Destructible.cs
+++ Assets/4 - Explosions/Scripts/Destructible.cs
@@ -1,15 +1,22 @@
+using System.Collections;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
 
 public class Destructible : MonoBehaviour
 {
     [SerializeField] private float maximumHealth = 100f;
     [SerializeField] private Renderer renderer;
+    [SerializeField] private TMP_Text text;
+    [SerializeField] private float dmgFeedbackDuration = 0.5f;
     
     [SerializeField][ReadOnly] private float currentHealth;
 
     private Rigidbody rb;
     private Collider collider;
     private Color originalColor;
+    private Coroutine feedbackRoutine;
+    
     public float HealthPercentage => currentHealth / maximumHealth;
     public Collider Collider => collider;
 
@@ -19,6 +26,7 @@ public class Destructible : MonoBehaviour
         collider = GetComponent<Collider>();
         currentHealth = maximumHealth;
         originalColor = renderer.material.color;
+        text.enabled = false;
     }
 
     public void ApplyForce(Vector3 direction, float force, float damage, ForceMode mode = ForceMode.Force, bool showLogs = false)
@@ -32,6 +40,7 @@ public class Destructible : MonoBehaviour
     public void ApplyDamage(float damage)
     {
         currentHealth -= damage;
+        ShowHealthStatus();
         var bodyColor = renderer.material.color;
         bodyColor.r = HealthPercentage * originalColor.r;
         bodyColor.b = HealthPercentage * originalColor.b;
@@ -41,6 +50,20 @@ public class Destructible : MonoBehaviour
         {
             Destroy();
         }
+    }
+
+    private IEnumerator ShowHealthStatusCoroutine()
+    {
+        text.text = ((int)(HealthPercentage*100f)).ToString(CultureInfo.InvariantCulture) + "%";
+        text.enabled = true;
+        yield return new WaitForSeconds(dmgFeedbackDuration);
+        text.enabled = false;
+    }
+
+    public void ShowHealthStatus()
+    {
+        if (feedbackRoutine != null) StopCoroutine(feedbackRoutine);
+        feedbackRoutine = StartCoroutine(ShowHealthStatusCoroutine());
     }
     
     public void Destroy()
